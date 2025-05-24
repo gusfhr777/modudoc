a76ba3faa6cfd5225c9b88503455a37ade7aed6f
gusfhr777 <53698707+gusfhr777@users.noreply.github.com>
2025-05-24 15:33:24 +0900

    Server network init

--- server/src/main/java/com/piltong/modudoc/server/ServerApp.java
+++ server/src/main/java/com/piltong/modudoc/server/ServerApp.java
@@ -7,16 +7,25 @@ import com.piltong.modudoc.server.network.ServerNetworkHandler;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // 서버 앱의 메인 클래스
-// 단순히 서버 프로그램의 시작점 역할(부트스트래핑)을 하며, 핵심 비즈니스 로직은 타 컴포넌트(서비스, 핸들러, 컨트롤러 등)에 위임한다.
+// 단순히 서버 프로그램의 시작점 역할(부트스트래핑)을 한다.
+// 핵심 비즈니스 로직은 타 컴포넌트(서비스, 핸들러, 컨트롤러 등)에 위임한다.
 public class ServerApp {
     public static void main(String[] args) {
-        // 시작
-        System.out.println("서버 가동중");
 
-        // ServerNNetworkHandler 생성 및 시작
-        ServerNetworkHandler networkHandler = new ServerNetworkHandler(Constants.SERVER_PORT);
+        // 스레드 풀 생성
+        // 스레드 풀 : 앱에서 사용하는 스레드 개수를 관리하는 기법. https://engineerinsight.tistory.com/197
+        ExecutorService executor = Executors.newFixedThreadPool(Constants.POOL_SIZE);
+
+        // ServerNetworkHandler 스레드 생성 및 시작
+        Runnable networkHandler = new ServerNetworkHandler(Constants.SERVER_PORT, executor);
+        new Thread(networkHandler).start();
+
+        // 시작
+        System.out.println("Server Stared on Port " + Constants.SERVER_PORT);
 
     }
 }
--- server/src/main/java/com/piltong/modudoc/server/network/ClientHandler.java
+++ server/src/main/java/com/piltong/modudoc/server/network/ClientHandler.java
@@ -1,9 +1,53 @@
 package com.piltong.modudoc.server.network;
 
 
-// 클라이언트 관련 내용을 처리한다.
-public class ClientHandler {
-    public ClientHandler() {
+import java.net.*;
+import java.io.*;
+import java.util.Objects;
+
+// 한 클라이언트와의 소켓 통신을 관리한다.
+// 요청 읽기 -> 비즈니스 로직 수행 -> 출력 스트림으로 응답 전송
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+
+    public ClientHandler(Socket socket) {
+        // 변수 할당
+        this.socket = socket;
 
     }
+
+    @Override
+    public void run() {
+        try {
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            out = new ObjectOutputStream(os);
+            in = new ObjectInputStream(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void HandleRequest() {
+
+    }
+
+
+    // 핸들러 종료
+    public void shutdown() {
+
+        try {
+            if (in != null) in.close();
+            if (out != null) out.close();
+            if (socket != null && !socket.isClosed()) socket.close();
+            System.out.println("Connection closed: " + Objects.requireNonNull(socket).getRemoteSocketAddress());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
