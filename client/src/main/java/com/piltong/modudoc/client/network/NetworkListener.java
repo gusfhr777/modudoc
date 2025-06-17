dfa797bb243be3e9b26bd2c4fc2411e1e2d4c5f8
yoonsehyun <jacknysh@gmail.com>
2025-06-17 17:19:59 +0900

    Create NetworkListener.java

---
+++ client/src/main/java/com/piltong/modudoc/client/network/NetworkListener.java
@@ -0,0 +1,87 @@
+package com.piltong.modudoc.client.network;
+
+import com.piltong.modudoc.client.controller.DocumentListController;
+import com.piltong.modudoc.client.controller.TextEditorController;
+import com.piltong.modudoc.common.document.Document;
+import com.piltong.modudoc.common.document.DocumentSummary;
+import com.piltong.modudoc.common.network.ClientCommand;
+import com.piltong.modudoc.common.network.ClientNetworkListener;
+import com.piltong.modudoc.common.operation.Operation;
+
+import java.util.List;
+
+public class NetworkListener implements ClientNetworkListener {
+
+    DocumentListController documentListController;
+    TextEditorController textEditorController;
+
+    @Override
+    public <T> void onCommandSuccess(ClientCommand command, T payload) {
+        // 성공 응답
+        switch (command) {
+
+            // 문서 생성 명령
+            case CREATE_DOCUMENT:
+                documentListController.
+                break;
+
+            // 단일 문서 조회 명령
+            case READ_DOCUMENT:
+                documentListController.
+                break;
+
+            // 문서 수정 명령
+            case UPDATE_DOCUMENT:
+                listener.onCommandSuccess(command, null);
+                break;
+
+            // 문서 삭제 명령
+            case DELETE_DOCUMENT:
+                listener.onCommandSuccess(command, null);
+                break;
+
+            // 문서 요약 리스트 조회 명령
+            case READ_DOCUMENT_SUMMARIES:
+                listener.onCommandSuccess(command, (List<DocumentSummary>) dto.getPayload());
+                break;
+
+            // Operation 전파 명령
+            case PROPAGATE_OPERATION:
+                listener.onCommandSuccess(command, null);
+                break;
+
+            // 이외 명령
+            default:
+                System.err.println("Unknown Command Received: " + command);
+                break;
+        }
+    }
+
+    @Override
+    public void onOperationReceived(Operation op) {
+
+    }
+
+    @Override
+    public void onCommandFailure(ClientCommand command, String errorMessage) {
+
+    }
+
+    @Override
+    public void onNetworkError(Throwable t) {
+
+    }
+
+    public void setDocumentListController(DocumentListController documentListController) {
+        this.documentListController = documentListController;
+    }
+    public void deleteDocumentListController() {
+        this.documentListController = null;
+    }
+    public void setTextEditorController(TextEditorController textEditorController) {
+        this.textEditorController = textEditorController;
+    }
+    public void deleteTextEditorController() {
+        this.textEditorController = null;
+    }
+}
